b5634b06ecee7eeeb3524cc589b6d18964ff3918
Raphaël Vinot <raphael@vinot.info>
2024-05-24 17:40:22 +0200

    chg: Support for valkey

--- cache/run_redis.sh
+++ cache/run_redis.sh
@@ -3,4 +3,11 @@
 set -e
 set -x
 
-../../redis/src/redis-server ./cache.conf
+if [ -f  ../../valkey/src/valkey-server ]; then
+    ../../valkey/src/valkey-server ./cache.conf
+elif [ -f ../../redis/src/redis-server ]; then
+    ../../redis/src/redis-server ./cache.conf
+else
+    echo "Warning: using system redis-server. Valkey-server or redis-server from source is recommended." >&2
+    /usr/bin/redis-server ./cache.conf
+fi
